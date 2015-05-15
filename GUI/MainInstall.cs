6ab3b376e846d7fd5a8b63e231ec597c5fb9efbb
Richard Lake <richard.lake@gmail.com>
2015-05-15 21:01:54 +0930

    General cleanup of MainInstall

--- GUI/MainInstall.cs
+++ GUI/MainInstall.cs
@@ -8,47 +8,44 @@ using System.Windows.Forms;
 
 namespace CKAN
 {
-    /** WARNING **/
-    // Much of this file does not correctly use Util.Invoke. Never call from a thread other than the UI
-    /** WARNING **/
+    using ModWithChangeType = KeyValuePair < CkanModule, GUIModChangeType>;
 
-    public class MainInstall
+    internal class MainInstall
     {
-        private KeyValuePair<List<KeyValuePair<CkanModule, GUIModChangeType>>, RelationshipResolverOptions> opts;
-        public HashSet<string> to_install;
+        private readonly List<ModWithChangeType> mods_with_changes;
+
         private readonly KSPVersion version;
         private readonly Registry registry;
-        private HashSet<string> to_upgrade;
-        private HashSet<string> to_uninstall;
         private readonly ModuleInstaller installer;
+
+        private readonly HashSet<string> to_install = new HashSet<string>();
+        private readonly HashSet<string> to_upgrade = new HashSet<string>();
+        private readonly HashSet<string> to_uninstall = new HashSet<string>();
+
         public readonly Dictionary<string, List<string>> recommended = new Dictionary<string, List<string>>();
         public readonly Dictionary<string, List<string>> suggested = new Dictionary<string, List<string>>();
 
         /// <summary>
-        ///
+        /// 
         /// </summary>
-        /// <param name="opts"></param>
+        /// <param name="mods_with_changes"></param>
         /// <param name="installer">Assumes that the User properly uses the invoke methods</param>
         /// <param name="registry"></param>
         /// <param name="version"></param>
         public MainInstall(
-            KeyValuePair<List<KeyValuePair<CkanModule, GUIModChangeType>>, RelationshipResolverOptions> opts,
+            List<ModWithChangeType> mods_with_changes,
             ModuleInstaller installer, Registry registry, KSPVersion version)
         {
             this.registry = registry;
             this.version = version;
-            this.opts = opts;
+            this.mods_with_changes = mods_with_changes;
             this.installer = installer;
         }
 
         public void Start()
         {
-            to_install = new HashSet<string>();
-            to_uninstall = new HashSet<string>();
-            to_upgrade = new HashSet<string>();
-
             // First compose sets of what the user wants installed, upgraded, and removed.
-            foreach (var change in opts.Key)
+            foreach (var change in mods_with_changes)
             {
                 switch (change.Value)
                 {
@@ -64,12 +61,10 @@ namespace CKAN
                 }
             }
 
-            // Now work on satisifying dependencies.
-
-
+            //Construct the lists of recommended and suggested mods. 
             foreach (
                 var ckan_module in
-                    opts.Key.Where(change => change.Value == GUIModChangeType.Install).Select(change => change.Key))
+                    mods_with_changes.Where(change => change.Value == GUIModChangeType.Install).Select(change => change.Key))
             {
                 AddInstallableModsToDict(version, recommended, ckan_module.recommends, ckan_module.identifier);
                 AddInstallableModsToDict(version, suggested, ckan_module.suggests, ckan_module.identifier);
@@ -100,7 +95,7 @@ namespace CKAN
                         !registry.IsInstalled(mod.name) &&
                         !to_install.Contains(mod.name))
                     {
-                        // add it to the list of mods we display to the user
+                        // add it to the list of mods we display to the user                                    
                         dictionary.AppendItemOrAddNewList(mod.name, endorser);
                     }
                 }
@@ -204,38 +199,66 @@ namespace CKAN
             }
             return true;
         }
+
+        public void Add(string identifier)
+        {
+            to_install.Add(identifier);
+    }
     }
 
 
     public class MainInstallGUI
     {
+        private delegate void MainCancelCallback();
 
-        public Main main;
-        public KSP ksp_instance;
-        internal BackgroundWorker m_InstallWorker;
+        private MainCancelCallback cancel_callback;
+        private readonly Main main;
+        private readonly KSP ksp_instance;
+        internal BackgroundWorker install_worker;
 
 
         // used to signal the install worker that the user canceled the install process
         // this may happen on the recommended/suggested mods dialogs
         private volatile bool install_canceled;
 
-        // this will be the final list of mods we want to install
+        // this will be the final list of mods we want to install 
         private MainInstall main_install;
+        private readonly TabController tab_controller;
 
-        public MainInstallGUI(KSP ksp_instance,Main main)
+        public MainInstallGUI(KSP ksp_instance, Main main, TabController tab_controller)
         {
             this.main = main;
             this.ksp_instance = ksp_instance;
+            this.tab_controller = tab_controller;
+
+            install_worker = new BackgroundWorker {WorkerReportsProgress = true, WorkerSupportsCancellation = true};
+            install_worker.DoWork += InstallMods;
+
+            // TODO. The remaining usages of main are as follows.            
+            // main.ClearLog();
+            // main.SetDescription()
+            // main.AddStatusMessage()
+            // main.CancelCurrentActionButton1.Enabled = false;
+            // main.HideWaitDialog(true);
+            //
+            // main.ChooseProvidedModsLabel.Text
+            // main.ChooseProvidedModsListView.Items.Clear()
+            // main.ChooseProvidedModsListView.ItemChecked
+            //
+            // main.RecommendedDialogLabel.Text
+            // main.RecommendedModsListView.Columns[1].Text
+            // main.RecommendedModsListView.Items.Clear()
+            // main.RecommendedModsListView.Items.Add(item)
+            // Consider either a move of these into this class or grouping them into interfaces which are passed in. 
         }
 
 
-        internal void InstallMods(object sender, DoWorkEventArgs e) // this probably needs to be refactored
+        private void InstallMods(object sender, DoWorkEventArgs e)
         {
-            var opts =
-                (KeyValuePair<List<KeyValuePair<CkanModule, GUIModChangeType>>, RelationshipResolverOptions>) e.Argument;
+            var opts = (KeyValuePair<List<ModWithChangeType>, RelationshipResolverOptions>) e.Argument;
 
-            main.ClearLog();
-            main_install = new MainInstall(opts, ModuleInstaller.GetInstance(ksp_instance, GUI.user),
+            main.ClearLog();            
+            main_install = new MainInstall(opts.Key, ModuleInstaller.GetInstance(ksp_instance, GUI.user),
                 RegistryManager.Instance(ksp_instance).registry, ksp_instance.Version());
             main_install.Start();
 
@@ -244,21 +267,21 @@ namespace CKAN
 
             if (install_canceled)
             {
-                main.m_TabController.HideTab("WaitTabPage");
-                main.m_TabController.ShowTab("ManageModsTabPage");
-                e.Result = new KeyValuePair<bool, List<KeyValuePair<CkanModule, GUIModChangeType>>>(false, opts.Key);
+                tab_controller.HideTab("WaitTabPage");
+                tab_controller.ShowTab("ManageModsTabPage");
+                e.Result = new KeyValuePair<bool, List<ModWithChangeType>>(false, opts.Key);
                 return;
             }
 
             // Now let's make all our changes.
 
-            main.m_TabController.RenameTab("WaitTabPage", "Installing mods");
-            main.m_TabController.ShowTab("WaitTabPage");
-            main.m_TabController.SetTabLock(true);
+            tab_controller.RenameTab("WaitTabPage", "Installing mods");
+            tab_controller.ShowTab("WaitTabPage");
+            tab_controller.SetTabLock(true);
 
 
             var downloader = new NetAsyncDownloader(GUI.user);
-            main.cancelCallback = () =>
+            cancel_callback = () =>
             {
                 downloader.CancelDownload();
                 install_canceled = true;
@@ -283,22 +306,21 @@ namespace CKAN
             {
                 if (install_canceled)
                 {
-                    e.Result = new KeyValuePair<bool, List<KeyValuePair<CkanModule, GUIModChangeType>>>(false,
+                    e.Result = new KeyValuePair<bool, List<ModWithChangeType>>(false,
                         opts.Key);
                     return;
                 }
                     var ret = main_install.InstallList(opts.Value, downloader, GUI.user, ModuleInstaller.GetInstance(ksp_instance, GUI.user));
                     if (!ret)
                     {
-                        // install failed for some reason, error message is already displayed to the user
-                        e.Result = new KeyValuePair<bool, List<KeyValuePair<CkanModule, GUIModChangeType>>>(false,
+                        // install failed for some reason, error message is already displayed to the user                    
+                        e.Result = new KeyValuePair<bool, List<ModWithChangeType>>(false,
                             opts.Key);
                         return;
                     }
                     resolved_all_provided_mods = true;
                 }
 
-            e.Result = new KeyValuePair<bool, List<KeyValuePair<CkanModule, GUIModChangeType>>>(true, opts.Key);
         }
 
         private void DisplayModSelectionTab(Dictionary<string, List<string>> modlist, string tab_name)
@@ -309,19 +331,16 @@ namespace CKAN
             {
                 Util.Invoke(main, () => UpdateRecommendedDialog(modlist));
 
-                main.m_TabController.ShowTab("ChooseRecommendedModsTabPage", 3);
-                main.m_TabController.RenameTab("ChooseRecommendedModsTabPage", tab_name);
-                main.m_TabController.SetTabLock(true);
+                tab_controller.ShowTab("ChooseRecommendedModsTabPage", 3);
+                tab_controller.RenameTab("ChooseRecommendedModsTabPage", tab_name);
+                tab_controller.SetTabLock(true);
 
-                lock (this)
-                {
-                    Monitor.Wait(this);
-                }
+                LockAndWait();
 
-                main.m_TabController.SetTabLock(false);
+                tab_controller.SetTabLock(false);
             }
 
-            main.m_TabController.HideTab("ChooseRecommendedModsTabPage");
+            tab_controller.HideTab("ChooseRecommendedModsTabPage");
         }
 
 
@@ -335,7 +354,7 @@ namespace CKAN
             Util.Invoke(main.ChooseProvidedModsLabel, () =>
             {
                 main.ChooseProvidedModsLabel.Text = String.Format(
-                    "Module {0} is provided by more than one available module, please choose one of the following mods:",
+                "Module {0} is provided by more than one available module, please choose one of the following mods:",
                 too_many_provides.requested);
                 main.ChooseProvidedModsListView.Items.Clear();
             });
@@ -378,16 +397,14 @@ namespace CKAN
         internal void ChooseProvidedModsCancelButton_Click(object sender, EventArgs e)
         {
             toomany_source.SetResult(null);
-            }
+        }
 
         internal void ChooseProvidedModsContinueButton_Click(object sender, EventArgs e)
         {
-            foreach (ListViewItem item in main.ChooseProvidedModsListView.Items)
-            {
-                if (item.Checked)
-                {
-                    toomany_source.SetResult((CkanModule)item.Tag);
-            }
+            toomany_source.SetResult(
+                main.ChooseProvidedModsListView.Items.Cast<ListViewItem>()
+                    .First(item => item.Checked)
+                    .Select(item => item.Tag));
             }
         }
 
@@ -406,7 +423,6 @@ namespace CKAN
                 main.RecommendedModsListView.Columns[1].Text = "Suggested by:";
             }
 
-
             main.RecommendedModsListView.Items.Clear();
 
             foreach (var pair in mods)
@@ -415,7 +431,7 @@ namespace CKAN
 
                 try
                 {
-                    var opts = new RelationshipResolverOptions()
+                    var opts = new RelationshipResolverOptions
                     {
                         with_all_suggests = false,
                         with_recommends = false,
@@ -468,20 +484,40 @@ namespace CKAN
                 if (item.Checked)
                 {
                     var identifier = ((CkanModule) item.Tag).identifier;
-                    main_install.to_install.Add(identifier);
+                    main_install.Add(identifier);
                 }
             }
 
-            lock (this)
-            {
-                Monitor.Pulse(this);
-            }
+            LockAndPulse();
         }
 
         internal void RecommendedModsCancelButton_Click(object sender, EventArgs e)
         {
             install_canceled = true;
 
+            LockAndPulse();
+        }
+
+        public void CancelCurrentActionButton_Click(object sender, EventArgs e)
+        {
+            if (cancel_callback != null)
+            {
+                cancel_callback();
+                main.CancelCurrentActionButton1.Enabled = false;
+                main.HideWaitDialog(true);
+                }
+            }
+
+        private void LockAndWait()
+        {
+            lock (this)
+            {
+                Monitor.Wait(this);
+            }
+        }
+
+        private void LockAndPulse()
+        {
             lock (this)
             {
                 Monitor.Pulse(this);
